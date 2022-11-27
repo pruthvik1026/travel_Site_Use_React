cfef66e96152757ccd46881dc6d85f11e622a037
pruthvik1025 <pruthvik.sarkheliya.22@mountblue.tech>
2022-11-27 12:33:30 +0530

    till heroSection done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom'
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route path='/' exact />
+                </Switch>
+            </Router>
+        </>
+  );
+}
+
+export default App;
